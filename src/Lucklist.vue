2470ae049ba40abc5f409cfd455b3ba2c80ddde9
cuifeng <cuifeng.li@dianjoy.com>
2016-05-12 15:00:33 +0800

    增加markdown的例子

--- src/Lucklist.vue
+++ src/Lucklist.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="info-div">
-    <div class="child" v-show="show" transition="bonuce">
+    <div class="child" v-if="true" >
       <ul>
-        <li v-for="one in list">{{one.name}} 抽中 {{one.prize}}</li>
+        <li v-for="one in list" id="{{$index}}" :transition="$index<30?'':transitionName">{{one.name}} 抽中 {{one.prize}}</li>
       </ul>
     </div>
   </div>
@@ -11,11 +11,15 @@
   </div>
   <helloworld></helloworld>
   <guodu></guodu>
+
+  <markdown></markdown>
 </template>
 
 <script>
   import Helloworld from './components/Helloworld.vue'
   import Guodu from './components/Guodu.vue'
+  import markdown from './components/markdown.vue'
+  
   export default{
     computed: {
       b: function () {
@@ -24,96 +28,70 @@
     },
     components: {
       helloworld: Helloworld,
-      Guodu: Guodu
+      Guodu: Guodu,
+      markdown: markdown
     },
     data () {
       return {
         toppx: 0,
         a: 1,
+        transitionName: 'bonuce',
         show: true,
         newList: [],
-        list: [{name: 'test', prize: 'prize1'},
-                {name: 'name2', prize: 'prize2'},
-                {name: 'name3', prize: 'prize3'},
-                {name: 'name4', prize: 'prize41'},
-                {name: 'name4', prize: 'prize42'},
-                {name: 'name4', prize: 'prize43'},
-                {name: 'name4', prize: 'prize44'},
-                {name: 'name4', prize: 'prize45'},
-                {name: 'name4', prize: 'prize46'},
-                {name: 'name4', prize: 'prize47'},
-                {name: 'name4', prize: 'prize48'},
-                {name: 'name4', prize: 'prize49'},
-                {name: 'name4', prize: 'prize411'},
-                {name: 'name4', prize: 'prize412'},
-                {name: 'name4', prize: 'prize413'},
-                {name: 'name4', prize: 'prize414'},
-                {name: 'name4', prize: 'prize415'},
-                {name: 'name4', prize: 'prize416'},
-                {name: 'name4', prize: 'prize417'},
-                {name: 'name4', prize: 'prize418'},
-                {name: 'name4', prize: 'prize419'},
-                {name: 'name4', prize: 'prize420'},
-                {name: 'name4', prize: 'prize421'},
-                {name: 'name4', prize: 'prize422'},
-                {name: 'name4', prize: 'prize423'},
-                {name: 'name4', prize: 'prize424'},
-                {name: 'name4', prize: 'prize425'},
-                {name: 'name4', prize: 'prize426'},
-                {name: 'name4', prize: 'prize427'},
-                {name: 'name4', prize: 'prize428'},
-                {name: 'name4', prize: 'prize429'},
-                {name: 'name4', prize: 'prize430'},
-                {name: 'name4', prize: 'prize431'},
-                {name: 'name4', prize: 'prize432'},
-                {name: 'name4', prize: 'prize433'},
-                {name: 'name4', prize: 'prize434'}]
+        list: []
       }
     },
     methods: {
       add: function () {
+        this.show = false
         var name1 = 'name1' + new Date().getTime()
         this.list.push({name: name1, prize: 'prize'})
         var name2 = 'name2' + new Date().getTime()
         this.list.push({name: name2, prize: 'prize'})
+
         setTimeout(this.add, 5000)
       }
     },
     transitions: {
       bonuce: {
         beforeEnter: function (el) {
+          if (el.id <= 38) {
+
+          }
+        },
+        enterCancelled: function (el) {
+          el.id <= 30
         },
         beforeLeave: function (el) {
+          console.log(el.id)
+        },
+        leaveCancelled: function (el) {
+          el.id <= 30
         }
       }
 
     },
     ready: function () {
+      this.show = true
       setTimeout(this.add, 5000)
     }
   }
 </script>
 <style>
   .bonuce-transition{
-    animation: mymove 5s linear;
-    animation-iteration-count: 1;
-
+    transition: margin 3s;
+    margin-top: -20px;
   }
 
   .bonuce-enter{
-    display: block;
+    margin-top: 0px;
   }
   .bonuce-leave{
-    display: none;
-  }
-
-  @keyframes mymove {
-    from {margin-top: 0px;}
-    to {margin-top: -320px;}
+    margin-top: 0px;
   }
 
   .info-div{
-    height: 100px;
+    height: 500px;
     border: 1px solid #ff0000;
     margin: 0 auto;
     position: relative;
---
+++ src/components/markdown.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class='parent' id="editor">
+    <textarea v-model="input" debonuce="300"></textarea>
+    <div v-html="input | marked">
+    </div>
+  </div>
+</template>
+<script>
+  var marked = require('marked') // 此处需要采用npm install marked进行安装
+  export default {
+    data () {
+      return {
+        input: '# hello'
+      }
+    },
+    filters: {
+      marked: marked
+    }
+  }
+</script>
+<style>
+  #editor{
+
+    height: 100%;
+    font-family: 'Helvetica Neue', Arial, sans-serif;
+    color: #333;
+  }
+  textarea, #editor div{
+    display: inline-block;
+    width: 49%;
+    height: 100%;
+    vertical-align: top;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 0 20px;
+  }
+
+  textarea {
+    border: none;
+    border-right: 1px solid #ccc;
+    risize: none;
+    outline: none;
+    background-color: #f6f6f6;
+    font-size: 14px;
+    font-family: 'Monaco', courier, monospace;
+    padding: 20px;
+  }
+
+  code {
+    color: #f66;
+  }
+</style>
